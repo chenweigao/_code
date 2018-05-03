78f523a556b70b58fc661e2f4270dbea5a40e5d0
chenweigao <297859260@qq.com>
2018-05-03 09:50:41 +0800

    del django file, to another directory

--- djangosite/app/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-form app.models import Moment
-
-class MomentForm(ModelForm):
-    class Mata:
-        models = Moment
-        fields = '__all__'
--- djangosite/app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def welcome(request):
-    return HttpResponse("<h1>welcome!</h1>")
