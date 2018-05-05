2762f11a8158c1549f3a55c8d38a3410e00b6bff
chenweigao <297859260@qq.com>
2018-05-05 15:17:28 +0800

    new lambda

--- newcpp_lambda.cpp
+++ newcpp_lambda.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 
+
 auto f1 = [](int x) {
     return x*x;
+};
+//表达式结束需要用分号
+auto f2 = [](std::string s) {
+    std::cout << s << std::endl;
+};
+
+auto f3 = [](int x, int y) {
+    return x < y;
 };
 
 int main() {
     std::cout << f1(3) << std::endl;
+    f2("heavy rain");
+    std::cout << f3(1, 5) << std::endl;
 }
