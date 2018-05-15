32e398f0e5769a61000012d66ca4b94856ea1400
chenweigao <297859260@qq.com>
2018-05-15 15:19:39 +0800

    bst

--- Test_C++/BST2.cpp
+++ Test_C++/BST2.cpp
@@ -2,49 +2,60 @@
 #include <vector>
 using namespace std;
 
-struct TreeNode {
+struct TreeNode
+{
     char val;
-    int count=1;
+    int count = 1;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(char val){
-        this -> val = val;
+    TreeNode(char val)
+    {
+        this->val = val;
     }
 };
-ostream& operator<<(ostream& st, TreeNode& node){
+ostream &operator<<(ostream &st, TreeNode &node)
+{
     st << node.val << ':' << node.count;
     return st;
 }
-class  Solution{
-public:
+class Solution
+{
+  public:
     TreeNode *root;
 
-    void insert(int val, TreeNode * & root){
-//        TreeNode *node = new TreeNode(val);
-        if(root == nullptr){
+    void insert(int val, TreeNode *&root)
+    {
+        //        TreeNode *node = new TreeNode(val);
+        if (root == nullptr)
+        {
             root = new TreeNode(val);
         }
 
-        else if(val == root->val){
-            root -> count += 1;
+        else if (val == root->val)
+        {
+            root->count += 1;
         }
 
-        else if(val < root->val){
+        else if (val < root->val)
+        {
             insert(val, root->left);
         }
         else
             insert(val, root->right);
     }
 
-    void  create(vector<char> &a){
-        for(int val : a)
+    void create(vector<char> &a)
+    {
+        for (int val : a)
         {
             insert(val, this->root);
         }
     }
 
-    void traverse(vector<TreeNode> &res, TreeNode* root){
-        if(root == nullptr){
+    void traverse(vector<TreeNode> &res, TreeNode *root)
+    {
+        if (root == nullptr)
+        {
             return;
         }
         traverse(res, root->left);
@@ -52,22 +63,24 @@ public:
         traverse(res, root->right);
     }
 
-    vector<TreeNode> inorderTraverse(){
+    vector<TreeNode> inorderTraverse()
+    {
         vector<TreeNode> res;
         traverse(res, root);
-        return  res;
+        return res;
     }
-
 };
-int main() {
-    vector<char> a = {'a', 'b','a'};
+int main()
+{
+    vector<char> a = {'a', 'b', 'a'};
     auto t = new Solution();
     t->create(a);
     auto answer = t->inorderTraverse();
-    for(auto i: answer){
-        cout << i <<endl;
+    for (auto i : answer)
+    {
+        cout << i << endl;
     }
 
-//    std::cout << "Hello, World!" << std::endl;
+    //    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
