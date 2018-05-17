42f4f7c239aaf7d84162ce39e6ab4f60a1c47c37
chenweigao <297859260@qq.com>
2018-05-17 13:42:50 +0800

    reset

--- LeetCode/TIQ_Array1.cpp
+++ LeetCode/TIQ_Array1.cpp
@@ -1,69 +1,27 @@
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solutiion
+{
+  public:
+    int removeDuplicates(vector<int> &nums)
+    {
         int slow = 1;
-        for(int fast = 1; fast < nums.size(); fast++){
-            if(nums[slow-1] != nums[fast]){
-                nums[slow++] = nums[fast]; 
+        for (int fast = 1; fast < nums.size(); fast++)
+        {
+            if (nums[slow - 1] != nums[fast])
+            {
+                nums[slow++] = nums[fast];
             }
         }
-        return slow < nums.size()? slow : nums.size();
+        return slow < nums.size() ? slow : nums.size();
     }
 };
-
-void trimLeftTrailingSpaces(string &input) {
-    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
-        return !isspace(ch);
-    }));
-}
-
-void trimRightTrailingSpaces(string &input) {
-    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
-        return !isspace(ch);
-    }).base(), input.end());
-}
-
-vector<int> stringToIntegerVector(string input) {
-    vector<int> output;
-    trimLeftTrailingSpaces(input);
-    trimRightTrailingSpaces(input);
-    input = input.substr(1, input.length() - 2);
-    stringstream ss;
-    ss.str(input);
-    string item;
-    char delim = ',';
-    while (getline(ss, item, delim)) {
-        output.push_back(stoi(item));
-    }
-    return output;
-}
-
-string integerVectorToString(vector<int> list, int length = -1) {
-    if (length == -1) {
-        length = list.size();
-    }
-
-    if (length == 0) {
-        return "[]";
-    }
-
-    string result;
-    for(int index = 0; index < length; index++) {
-        int number = list[index];
-        result += to_string(number) + ", ";
-    }
-    return "[" + result.substr(0, result.length() - 2) + "]";
-}
-
-int main() {
-    string line;
-    while (getline(cin, line)) {
-        vector<int> nums = stringToIntegerVector(line);
-        
-        int ret = Solution().removeDuplicates(nums);
-
-        string out = integerVectorToString(nums, ret);
-        cout << out << endl;
-    }
+int main()
+{
+    vector<int> nums = {0, 0, 1, 1, 2, 3, 4, 4};
+    int ret = Solutiion().removeDuplicates(nums);
+    cout << ret << endl;
     return 0;
 }
