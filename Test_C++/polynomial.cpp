2b3d6b21e16cc6a524ac354d82f65f9c2e496715
chenweigao <297859260@qq.com>
2018-05-24 22:31:52 +0800

    modify polynomial.cpp

--- Test_C++/polynomial.cpp
+++ Test_C++/polynomial.cpp
@@ -3,47 +3,51 @@
 
 typedef std::map<int, int> polynomial;
 
-polynomial merge(polynomial a, polynomial b){
+polynomial merge(polynomial a, polynomial b)
+{
     polynomial result;
-    for (auto i:a){
-        result[i.first]=i.second;
+    for (auto i : a)
+    {
+        result[i.first] = i.second;
     }
 
-    for (auto i:b){
-        result[i.first]=result[i.first]+i.second;
+    for (auto i : b)
+    {
+        result[i.first] = result[i.first] + i.second;
     }
 
     return result;
 }
 
-
-int main() {
+int main()
+{
     //std::cout << "Hello, World!" << std::endl;
 
-    polynomial a,b;
-    a[0]=0;
-    a[1]=1;
-    a[2]=2;
+    polynomial a, b;
+    a[0] = 13;
+    a.insert({{1, 5}, {2, 9}}); //from c++11
+    //13*x^0 + 5*x^1 + 9*x^2
 
-    b[1]=10;
-    b[2]=20;
-    b[3]=30;
+    b[1] = 10;
+    b[2] = 20;
+    b.insert(std::pair<int, int>(3, 30));
+    //10*x^1 + 20*x^2 + 30*x^3
+    auto c = merge(a, b);
 
-    auto c=merge(a, b);
-
-    bool plus=false;
-    for(auto i:c){
-        if(i.second==0)
+    bool plus = false;
+    for (auto i : c)
+    {
+        if (i.second == 0)
             continue;
 
-        if(plus)
-            std::cout<<" + ";
+        if (plus)
+            std::cout << " + ";
 
-        std::cout<<i.second<<"x^"<<i.first;
-        plus= true;
+        std::cout << i.second << "x^" << i.first;
+        plus = true;
     }
 
-    std::cout<<std::endl;
+    std::cout << std::endl;
 
     return 0;
 }
