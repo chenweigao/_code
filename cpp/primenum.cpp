0fb447620ac8636615b82afb25ebc0afc6c5cac6
chenweigao <297859260@qq.com>
2018-09-14 10:50:08 +0800

    update code

---
+++ cpp/primenum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+using namespace std;
+int countPrimes(int n) {
+	    if (n<=2) return 0;
+	    vector<bool> passed(n, false);
+	    int sum = 1;
+	    int upper = sqrt(n);
+	    for( int i=3; i<n; i+=2) {
+		    if(!passed[i]) {
+			    sum ++;
+			    if (i > upper) continue;
+			    for(int j = i*i; j <n; j+=i) {
+				    passed[j] = true;
+			    }
+		    }
+	    }
+	    return sum;
+}
+int main() {
+	int n = 10;
+	cout << countPrimes(n) << endl;
+}
