f8f5a09eb9d11d7edf023afb651c95c13208a7c4
chenweigao <297859260@qq.com>
2019-01-23 22:39:31 +0800

    1

---
+++ cpp/io_stream_function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::istream;
+
+istream &func(istream &is)
+{
+    std::string buf;
+    while (is >> buf)
+    {
+        std::cout << buf << std::endl;
+    }
+    is.clear();
+    return is;
+}
+
+int main(int argc, char const *argv[])
+{
+    istream &is = func(std::cin);
+    std::cout << is.rdstate() << std::endl;
+    return 0;
+}
