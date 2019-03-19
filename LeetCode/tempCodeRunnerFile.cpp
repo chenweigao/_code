d8aa6f1bd08863e158e80af99a74ce558189cbc6
chenweigao <297859260@qq.com>
2019-03-19 17:22:12 +0800

    delete

--- LeetCode/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-
-{
-    vector<vector<int>> triangle;
-    triangle.push_back({2});
--- LeetCode/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-        # if n == 0:
-        #     return 0
-        # if n == 1:
-        #     return nums[0]
