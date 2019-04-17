eb8c9c02dba2c02ff05c6262c222f05445ed2b2b
chenweigao <297859260@qq.com>
2019-04-17 22:36:47 +0800

    update code

--- LeetCode/LC416_partition_equal_subset_sum.cpp
+++ LeetCode/LC416_partition_equal_subset_sum.cpp
@@ -8,6 +8,35 @@ class Solution
   public:
     bool canPartition(vector<int> &nums)
     {
+        int sum = 0;
+        int n = nums.size();
+
+        for (int num : nums)
+        {
+            sum += num;
+        }
+
+        if ((sum & 1) == 1)
+        {
+            return false;
+        }
+
+        int target = sum /= 2;
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+
+        dp[0][0] = true;
+
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i][0] = true;
+        }
+
+        for (int j = 0; j < target + 1; j++)
+        {
+            dp[0][j] = false;
+        }
+
         
     }
 }
