d2f1ed24a90ae15bfc05b1d6e8f6c0df506d3035
chenweigao <297859260@qq.com>
2019-04-28 17:40:11 +0800

    add a code

---
+++ LeetCode/LC3_longest_substring_without_repeating.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        hashset = set()
+
+        res = 0
+        i, j = 0, 0
+
+        while i < n and j < n:
+            if s[j] not in hashset:
+                hashset.add(s[j])
+                j += 1
+                res = max(res, j - i)
+            else:
+                hashset.remove(s[i])
+                i += 1
+        return res                
+
+
