34d57829a75b4d8890f8f2750d3260613930cfef
chenweigao <297859260@qq.com>
2019-04-29 17:03:27 +0800

    add code

--- LeetCode/LC012_inter_to_roman.py
+++ LeetCode/LC012_inter_to_roman.py
@@ -1,3 +1,17 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        
+        roman_dic = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C',
+                     90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+
+        # new = sorted(roman_dic.items(), key=lambda item: item[0], reverse=True)
+        # print(dict(roman_dic))
+        res = ''
+        for key, value in roman_dic.items():
+            res += (num // key) * value
+            num %= key
+        return res
+
+test_nums = [3, 4, 9, 58, 1994]
+
+for num in test_nums:
+    print(Solution().intToRoman(num)) 
