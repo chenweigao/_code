b4781ab11d6c4bad19d7dec338e98a779c9515a1
weigao chen <weigao_1995@yeah.net>
2019-06-28 09:56:55 +0800

    Update binary_search.py

--- data_struct/binary_search.py
+++ data_struct/binary_search.py
@@ -1,20 +1,16 @@
-def binarySearch(alist, value):
-    first = 0
-    last = len(alist) - 1
+# 先小于后大于，先左边(l)后右边(r)
+def binarySearch(nums, target):
+    l, r = 0, len(nums) - 1
 
-    mid = (last // 2)
-
-    while binarySearch:
-        if value == alist[mid]:
-            return mid
+    while l <= r:
+        mid = (l + r) // 2
+        if nums[mid] == target:
+            return True
+        elif nums[mid] < target:
+            l = mid + 1
         else:
-            if value < mid:
-                last = mid - 1
-            else:
-                if value > mid:
-                    first = mid + 1
-        return -1
-
+            r = mid - 1
+    return False
 
 def binarySearch2(arr, l, r, x):
     if r >= l:
