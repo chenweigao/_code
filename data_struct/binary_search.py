517627aaafcc31520ae9f4b4b14c04b164eb506d
weigao chen <weigao_1995@yeah.net>
2019-06-28 09:47:20 +0800

    Update binary_search.py

--- data_struct/binary_search.py
+++ data_struct/binary_search.py
@@ -34,3 +34,17 @@ print(binarySearch2([1, 2, 3, 4, 5, 6], 0, 5, 3))
 alist = [-3, -5, 9, 10, 13]
 value = 11
 print(rIndex(alist, value))
+
+
+# 使用 bisect 库
+import bisect
+
+li = [1, 3, 4, 4, 6, 7]
+
+print(bisect.bisect(li, 4)) # 默认找到相同元素的最右侧 4
+print(bisect.bisect_left(li, 4)) # 左侧 2
+
+
+li2 = [1, 3, 4, 4, 6, 7]
+bisect.insort(li2, 5)
+print(li2) # 输出排序后的，类似于插入排序
